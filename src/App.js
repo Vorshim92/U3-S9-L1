bc5c47c6f28cc7df32b3dfc018ad8e9adcae6a31
Vorshim92 <vorshim92@gmail.com>
2024-03-11 17:43:37 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from "./shock.jpg";
 import "./App.css";
+import ButtonComponent from "./components/ButtonComponent";
+import ImageComponent from "./components/ImageComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <ImageComponent src={logo} alt="SHISH" classname="App-logo" />
         <p>FIRST REACTion, SHOCK!</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <ButtonComponent text="SHOCK!" id="shock" />
       </header>
     </div>
   );
---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,4 @@
+const ButtonComponent = function (props) {
+  return <button id={props.id}>{props.text}</button>;
+};
+export default ButtonComponent;
